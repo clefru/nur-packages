89f98247682bb2d5c9f36bcf8dd635f98e820072
Clemens Fruhwirth <clemens@endorphin.org>
2019-01-14 22:05:55 +0100

    Add grin-miner.nix and clean-up grin.nix

--- pkgs/grin/grin.nix
+++ pkgs/grin/grin.nix
@@ -6,16 +6,14 @@ with pkgs;
   }) rec {
   name = "grin-${version}";
   version = "0.5.2";
-  buildInputs = [ ncurses zeromq pkgconfig openssl cmake clang
-  zlib czmq ];
+  buildInputs = [ ncurses zeromq pkgconfig openssl cmake clang zlib czmq ];
 
-  srcGitHub = fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "mimblewimble";
     repo = "grin";
     rev = "ba994248ac7f4e4e9e24abb5c654051f81862779";
     sha256 = "00lmvhx5mmsna87vrdhaff8v8hqdhakr77ax1rarrqzjggpnm4gb";
   };
-  src = if builtins.pathExists ./srcx/grin then ./src/grin else srcGitHub;
   
   cargoSha256 = "19snmmrqb8ar5072jpq905asazanpm7fbpn8p9x023cfnlc2g1c4";
   
@@ -29,24 +27,6 @@ with pkgs;
     (cd source; patch -p1 < ${./grin.diff})
     gzip -cd ${grinwebwallet} > /tmp/wallet.tar'';
     
-#    cat .cargo/config
-#    ls -l /build/grin-0.4.1-vendor
-#    ls -l /build
-#    cat > .cargo/configk << EOF
-#[source.crates-io]
-#replace-with = "vendored-sources"
-#
-#[source."https://github.com/mimblewimble/rust-secp256k1-zkp"]
-#git = "https://github.com/mimblewimble/rust-secp256k1-zkp"
-#tag = "grin_integration_28"
-#replace-with = "vendored-sources"
-#
-#[source.vendored-sources]
-#directory = "/build/${name}-vendor"
-#EOF
-#'';
-
-  # Needed for tests
-  
+  # Needed for tests  
   LIBCLANG_PATH = "${llvmPackages.clang-unwrapped.lib}/lib";
 })
