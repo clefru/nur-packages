2033cce722d2abbd35b3d57a7d8a1a637ad41496
Clemens Fruhwirth <clemens@endorphin.org>
2019-02-05 10:21:58 +0100

    Add noise-suppression-for-voice

--- default.nix
+++ default.nix
@@ -4,4 +4,5 @@ self: pkgs: {
   grin-miner = pkgs.callPackage ./pkgs/grin/grin-miner.nix {};
   linux-sgx = pkgs.callPackage ./pkgs/linux-sgx {};
   rnnoise = pkgs.callPackage ./pkgs/rnnoise {};
+  noise-suppression-for-voice = pkgs.callPackage ./pkgs/noise-suppression-for-voice {};
 }
