649a61b35e7aefc8412048118a0c7bac911f6436
Clemens Fruhwirth <clemens@endorphin.org>
2019-02-05 12:22:11 +0100

    Add scanbuttond

--- default.nix
+++ default.nix
@@ -5,4 +5,5 @@ self: pkgs: {
   linux-sgx = pkgs.callPackage ./pkgs/linux-sgx {};
   rnnoise = pkgs.callPackage ./pkgs/rnnoise {};
   noise-suppression-for-voice = pkgs.callPackage ./pkgs/noise-suppression-for-voice {};
+  scanbuttond = pkgs.callPackage ./pkgs/scanbuttond {};
 }
