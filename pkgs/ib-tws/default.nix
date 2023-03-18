50bac454993db35114601cad6f2a1a3d724de36a
Clemens Fruhwirth <clemens@endorphin.org>
2023-03-18 05:25:36 +0100

    ib-tws: 10.21.1q -> 10.21.1r

--- pkgs/ib-tws/default.nix
+++ pkgs/ib-tws/default.nix
@@ -2,12 +2,12 @@
 with pkgs;
 
 let ibDerivation = stdenv.mkDerivation rec {
-  version = "10.21.1q";
+  version = "10.21.1r";
   pname = "ib-tws-native";
 
   src = fetchurl {
     url = "https://download2.interactivebrokers.com/installers/tws/latest-standalone/tws-latest-standalone-linux-x64.sh";
-    sha256 = "1pw93xbwr3bsfl95dhipsfxbcbkfdsxv5n455d2axpi3z0ldxnvk";
+    sha256 = "1fyzqbyz0af8514c1jlc422kwhn445wr7v10prix25z5mjh44cmy";
     executable = true;
   };
 
