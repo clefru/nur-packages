02f1f0f8eb2bb28885dfc17dac05019019514c81
Clemens Fruhwirth <clemens@endorphin.org>
2024-12-23 18:26:27 +0100

    ib-tws: Try the home passthrough

--- pkgs/ib-tws/default.nix
+++ pkgs/ib-tws/default.nix
@@ -39,7 +39,7 @@ let ibDerivation = stdenv.mkDerivation rec {
     # -Dsun.java2d.opengl=False not applied. Why would I disable that?
     # -Dswing.aatext=true applied
     mkdir $out/bin
-    sed -e s#__OUT__#$out# -e s#__JAVAHOME__#${pkgs.openjdk8.jre}/lib/openjdk/jre# -e s#__GTK__#${pkgs.gtk3}# -e s#__CCLIBS__#${pkgs.stdenv.cc.cc.lib}# ${./tws-wrap.sh} > $out/bin/ib-tws-native
+    sed -e s#__OUT__#$out# -e s#__JAVAHOME__#${pkgs.openjdk8.jre.home}# -e s#__GTK__#${pkgs.gtk3}# -e s#__CCLIBS__#${pkgs.stdenv.cc.cc.lib}# ${./tws-wrap.sh} > $out/bin/ib-tws-native
 
     chmod a+rx $out/bin/ib-tws-native
 
